b8f94737f9a1fd91248f749dd5b9fb4f592a7a25
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2022-07-14 08:20:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/doctoxer/DVaUpgraded.git /DVaUpgraded
+  git clone https://github.com/doctoxer/Dvaoff.git /Dvaoff
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DVaUpgraded
+  git clone $UPSTREAM_REPO /Dvaoff
 fi
-cd /DVaUpgraded
+cd /Dvaoff
 pip3 install -U -r requirements.txt
 echo "Starting DVa RoBot...."
 python3 bot.py
